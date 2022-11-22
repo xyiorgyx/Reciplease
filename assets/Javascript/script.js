1c402dd9264d7a5d39c3275ef400eeafc80c7ad0
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-11-22 09:43:10 -0500

    working on getting recipe api to function

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,66 +1,45 @@
-var repoList = document.querySelector(".searchResultContainer");
-var fetchButton = document.getElementById("searchBtn");
-var searchBar = document.querySelector(".searchBar");
+var searchResultContainer = document.querySelector('.searchResultContainer');
+var fetchButton = document.getElementById('searchBtn');
+var searchBar = document.querySelector('.searchBar')
 var apiKey = "&apiKey=67c5935d239e403fba7b639eaf1d6eaa";
-var recipeId =
-  // var userInput = searchBar.value;
-  function handleUserInput() {
-    var searchInput = input.value;
-    getRecipeId(searchInput);
-  };
+// var userInput = searchBar.value;
+function handleUserInput() {
+  var searchInput = input.value
+  getRecipeId(searchInput)
+}
 function getRecipeId(searchInput) {
-  var requestUrl =
-    "https://api.spoonacular.com/recipes/complexSearch?query=" +
-    "pasta" +
-    apiKey +
-    "&number=20";
+  var requestUrl = 'https://api.spoonacular.com/recipes/complexSearch?query=' + 'pasta' + apiKey + '&number=20';
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
-    })
-    .then(function (data) {
-      // console.log(data);
-      var recipeNameArray = [
-        data.results[0],
-        data.results[1],
-        data.results[2],
-        data.results[3],
-        data.results[4],
-        data.results[5],
-        data.results[6],
-        data.results[7],
-        data.results[8],
-        data.results[9],
-        data.results[10],
-        data.results[11],
-        data.results[12],
-        data.results[13],
-        data.results[14],
-        data.results[15],
-        data.results[16],
-        data.results[17],
-        data.results[18],
-        data.results[19],
-      ];
+    }).then(function (data) {
+      console.log(data);
+      var recipeNameArray = [data.results[0], data.results[1], data.results[2], data.results[3], data.results[4], data.results[5], data.results[6],
+      data.results[7], data.results[8], data.results[9], data.results[10], data.results[11], data.results[12], data.results[13], data.results[14], data.results[15],
+      data.results[16], data.results[17], data.results[18], data.results[19]]
       var recipeId = data.results[0].id;
-      //do a for loop to use the user input as the index for the recipe id var
-      console.log(recipeId);
-      console.log;
-    });
+      for (let i = 0; i < recipeNameArray.length; i++) {
+        var recipeTitle = recipeNameArray[i].results.title;
+        var recipePic = recipeNameArray[i].results.image;
+        var recipeId = recipeNameArray[i].results.id;
+      }
+    })
 }
 getRecipeId();
 // need to display the titles and maybe images for the recipe the user searches
 // then the user should be able to click the image or the title
 // from there it will need to grab the id of the recipe and the push it through the get recipeapi function below
-function getRecipeApi(recipeId) {
-  var recipeUrl =
-    "https://api.spoonacular.com/recipes/" + recipeId + "/information";
-  fetch(recipeUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-getRecipeApi();
+// function getRecipeApi(recipeId) {
+//   var recipeUrl = 'https://api.spoonacular.com/recipes/' + recipeId + '/information';
+//   fetch(recipeUrl)
+//     .then(function (response) {
+//       return response.json();
+//     }).then(function (data) {
+//       console.log(data);
+//     })
+// }
+// getRecipeApi();
+
+
+// need to add buttons
+// make a function that takes 
