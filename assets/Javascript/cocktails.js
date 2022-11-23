e84072070ac057e595229fea1acb5fb2b304e4eb
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-11-22 19:31:23 -0500

    drink.js

--- assets/Javascript/cocktails.js
+++ assets/Javascript/cocktails.js
@@ -2,17 +2,21 @@ var userInput = document.getElementById('userInput').value
 var requestUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}";
 
 function getApi() {
+    let requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
     fetch(requestUrl)
+   
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            // for (var i = 0; i < data.length; i++) {
-            //     var listItem = document.createElement('li');
-            //     listItem.textContent = data[i].html_url;
-            //     repoList.appendChild(listItem);
-            // }
+            
+            for (var i = 0; i < data.length; i++) {
+                console.log(data.drinks[0].strDrink)
+                var listItem = document.createElement('li');
+                listItem.textContent = data[i].html_url;
+                repoList.appendChild(listItem);
+            }
         });
 }
 getApi();
