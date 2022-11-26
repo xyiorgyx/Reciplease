6a413b68b2afb6fbfd1685d8d6b018285654928e
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-11-25 21:45:47 -0500

    Added elements to java script
    JavaScript now adds all necessary elements and their values for the fetched information onto the html files. However, this requires more tweaking as the structure is mixed.

--- assets/Javascript/cocktails.js
+++ assets/Javascript/cocktails.js
@@ -1,29 +1,55 @@
-var userInput = document.getElementById('userInput').value
-var requestUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}";
+var userInput = document.querySelector('#userInput').value
 
-function getApi() {
-    let requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
-    fetch(requestUrl)
-   
-        .then(function (response) {
+var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
+fetch(requestUrl)
+    .then((response) => {
+        if (response.ok) {
             return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+    .then(data => {
+        console.log(data);
+        displayCocktail(data)
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+    console.log(userInput);
+    
+function displayCocktail(data) {
+    
+    for (i=0; i < data.drinks.length; i++){
+
+    const cocktail = data.drinks[i];
+    const cocktailDiv = document.getElementById("cocktail");
+    const cocktailName = cocktail.strDrink;
+    const heading = document.createElement("h1");
+    heading.innerHTML = cocktailName;
+    cocktailDiv.appendChild(heading);
+    const cocktailImg = document.createElement("img");
+    cocktailImg.src = cocktail.strDrinkThumb;
+    cocktailDiv.appendChild(cocktailImg);
+    const cocktailIngredients = document.createElement("ul");
+    cocktailDiv.appendChild(cocktailIngredients);
+    
+    const getIngredients = Object.keys(cocktail)
+        .filter(function (ingredient) {
+            return ingredient.indexOf("strIngredient") == 0;
         })
-        .then(function (data) {
-            console.log(data);
-            
-            for (var i = 0; i < data.length; i++) {
-                console.log(data.drinks[0].strDrink)
-                var listItem = document.createElement('li');
-                listItem.textContent = data[i].html_url;
-                repoList.appendChild(listItem);
+        .reduce(function (ingredients, ingredient) {
+            if (cocktail[ingredient] != null) {
+                ingredients[ingredient] = cocktail[ingredient];
             }
-        });
+            return ingredients;
+        }, {});
+
+    for (let key in getIngredients) {
+        let value = getIngredients[key];
+        listItem = document.createElement("li");
+        listItem.innerHTML = value;
+        cocktailIngredients.appendChild(listItem);
+    }
 }
-getApi();
+}
 
-
-
-
-
-
-
+userInput.addEventListener('submit', displayCocktail)
