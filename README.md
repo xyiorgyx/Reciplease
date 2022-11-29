cd047a467f888485c47f2cc2c43c68a3c35c6b6c
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-11-28 20:16:30 -0500

    small fixes
    made some small fixes to the HTML's

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Reciplease
+
+Looking for something to cook for the family? Reciplease has a hundreds of recipes from different countries all around the world. Reciplease inludes: Main dishes, appetizers, and declicous desserts.
+
+## Directions
+
+## Live Link
+
+https://xyiorgyx.github.io/Reciplease/
+
+## Github Link
+
+https://github.com/xyiorgyx/Reciplease
+
+## Pictures
+
+
--- assets/Javascript/cocktails.js
+++ assets/Javascript/cocktails.js
@@ -1,98 +1,110 @@
 
 var userInput = document.querySelector('#userInput').value;
 var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
-var form  = document.getElementById('form');
+var form = document.getElementById('form');
 var submitButton = document.getElementById('submitBtn');
+const cocktailDiv = document.getElementById("cocktail");
 
-function FormSubmit(event){
-    event.preventDefault(event);
-    userInput = document.querySelector('#userInput').value
-    requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
-    fetch(requestUrl);
-
+function fetchDrinks(requestUrl) {
+    fetch(requestUrl)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("NETWORK RESPONSE ERROR");
+            }
+        })
+        .then(data => {
+            console.log(data);
+            displayCocktail(data)
+        })
+        .catch((error) => console.error("FETCH ERROR:", error));
 }
 
+
 console.log(requestUrl)
-fetch(requestUrl)
 
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("NETWORK RESPONSE ERROR");
-        }
-    })
-    .then(data => {
-        console.log(data);
-        displayCocktail(data)
-    })
-    .catch((error) => console.error("FETCH ERROR:", error));
 
+
+function FormSubmit(event) {
+    event.preventDefault(event);
+    cocktailDiv.innerHTML = ''
+    userInput = document.querySelector('#userInput').value
+    requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + userInput;
+    fetchDrinks(requestUrl);
+}
 
 
 function displayCocktail(data) {
+    if (!data.drinks) {
+        cocktailDiv.textContent = "No results found";
+    }
+    else {
+        for (i = 0; i < data.drinks.length; i++) {
 
-    for (i = 0; i < data.drinks.length; i++) {
+            const cocktail = data.drinks[i];
 
-        const cocktail = data.drinks[i];
-        const cocktailDiv = document.getElementById("cocktail");
-        const newDiv = document.createElement('div')
-        cocktailDiv.append(newDiv)
-        const drinkName = cocktail.strDrink;
-        const Title = document.createElement("h1");
-        Title.innerHTML = drinkName;
-        newDiv.appendChild(Title);
-        const cocktailImg = document.createElement("img");
-        cocktailImg.src = cocktail.strDrinkThumb;
-        newDiv.appendChild(cocktailImg);
-        const cocktailIngredients = document.createElement("ul");
-        // const cocktailMeasurements = document.innerHTML;
-        // cocktailIngredients.appendChild(cocktailMeasurements);
-        newDiv.appendChild(cocktailIngredients);
-        const directions = document.createElement('p');
-        directions.innerHTML = cocktail.strInstructions;
-        newDiv.appendChild(directions);
+            const newDiv = document.createElement('div')
+            cocktailDiv.append(newDiv)
+            const drinkName = cocktail.strDrink;
+            const Title = document.createElement("h1");
+            Title.innerHTML = drinkName;
+            newDiv.appendChild(Title);
+            const cocktailImg = document.createElement("img");
+            cocktailImg.src = cocktail.strDrinkThumb;
+            newDiv.appendChild(cocktailImg);
+            const cocktailIngredients = document.createElement("ul");
+            // const cocktailMeasurements = document.innerHTML;
+            // cocktailIngredients.appendChild(cocktailMeasurements);
+            newDiv.appendChild(cocktailIngredients);
+            const directions = document.createElement('p');
+            directions.innerHTML = cocktail.strInstructions;
+            newDiv.appendChild(directions);
 
 
-        const getIngredients = Object.keys(cocktail)
-            .filter(function (ingredient) {
-                return ingredient.indexOf("strIngredient") == 0;
-            })
-            .reduce(function (ingredients, ingredient) {
-                if (cocktail[ingredient] != null) {
-                    ingredients[ingredient] = cocktail[ingredient];
-                }
-                return ingredients;
-            }, {});
+            const getIngredients = Object.keys(cocktail)
+                .filter(function (ingredient) {
+                    return ingredient.indexOf("strIngredient") == 0;
+                })
+                .reduce(function (ingredients, ingredient) {
+                    if (cocktail[ingredient] != null) {
+                        ingredients[ingredient] = cocktail[ingredient];
+                    }
+                    return ingredients;
+                }, {});
 
-        for (let key in getIngredients) {
-            let value = getIngredients[key];
-            listItem = document.createElement("li");
-            listItem.innerHTML = value;
-            cocktailIngredients.appendChild(listItem);
+            for (let key in getIngredients) {
+                let value = getIngredients[key];
+                listItem = document.createElement("li");
+                listItem.innerHTML = value;
+                cocktailIngredients.appendChild(listItem);
+
+            }
+
+
+            // console.log(getIngredients)
+            //         const getMeasurements = Object.keys(cocktail)
+            //             .filter(function (measure) {
+            //                 return measure.indexOf("strMeasure") == 0;
+            //             })
+            //             .reduce(function (measures, measure) {
+            //                 if (cocktail[measure] != null) {
+            //                     measures[measure] = cocktail[measure];
+            //                 }
+            //                 return measures;
+            //             }, {});
+
+            //         for (let key in getMeasurements) {
+            //             let value = getMeasurements[key];
+            //             drinkItem = document.createElement("li");
+            //             drinkItem.innerHTML = value;
+            //             cocktailMeasurements.appendChild(drinkItem);
+            //         }
+            //         console.log(getMeasurements)
+            //     
         }
-
-
-        // const getMeasurements = Object.keys(cocktail)
-        //     .filter(function (measure) {
-        //         return measure.indexOf("strMeasure") == 0;
-        //     })
-        //     .reduce(function (measures, measure) {
-        //         if (cocktail[measure] != null) {
-        //             measures[measure] = cocktail[measure];
-        //         }
-        //         return measures;
-        //     }, {});
-
-        // for (let key in getMeasurements) {
-        //     let value = getMeasurements[key];
-        //     drinkItem = document.createElement("li");
-        //     drinkItem.innerHTML = value;
-        //     cocktailMeasurements.appendChild(drinkItem);
-        // }
-        // console.log(getMeasurements)
     }
 }
-
+fetchDrinks(requestUrl)
 form.addEventListener('submit', FormSubmit)
 submitButton.addEventListener('click', fetch)
