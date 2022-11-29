90312fe4c05343a12e086270325d6a3f36827141
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-11-28 21:29:41 -0500

    fixed issue with ingredients

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -10,87 +10,85 @@ var input = document.getElementById('default-search');
 var recipeHistory = JSON.parse(localStorage.getItem('savedRecipe')) || [];
 
 function handleUserInput(event) {
-    event.preventDefault();
-    getRecipeId(input.value)
+  event.preventDefault();
+  getRecipeId(input.value)
 }
 
 function getRecipeId(input) {
-    searchResultContainer.classList.remove('hidden')
-    recipeContainer.classList.add('hidden')
-    var requestUrl = 'https://api.spoonacular.com/recipes/complexSearch?query=' + input + davidsApiKey + '&number=20';
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            console.log(data);
+  searchResultContainer.classList.remove('hidden')
+  recipeContainer.classList.add('hidden')
+  var requestUrl = 'https://api.spoonacular.com/recipes/complexSearch?query=' + input + davidsApiKey + '&number=20';
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
 
-            searchResultContainer.innerHTML = ''
-            for (let i = 0; i < data.results.length; i++) {
-                var recipeTitle = data.results[i].title;
-                var recipePic = data.results[i].image;
-                var recipeId = data.results[i].id;
-                var div = document.createElement('div');
-                var liRecipeTitle = document.createElement('a');
-                liRecipeTitle.setAttribute('data-recipeId', recipeId)
-                liRecipeTitle.classList.add('titleId')
-                liRecipeTitle.textContent = recipeTitle
-                liRecipeTitle.addEventListener('click', userSelectRecipe)
-                div.append(liRecipeTitle)
-                searchResultContainer.append(div)
-            }
-        })
+      searchResultContainer.innerHTML = ''
+      for (let i = 0; i < data.results.length; i++) {
+        var recipeTitle = data.results[i].title;
+        var recipePic = data.results[i].image;
+        var recipeId = data.results[i].id;
+        var div = document.createElement('div');
+        var liRecipeTitle = document.createElement('a');
+        liRecipeTitle.setAttribute('data-recipeId', recipeId)
+        liRecipeTitle.classList.add('titleId')
+        liRecipeTitle.textContent = recipeTitle
+        liRecipeTitle.addEventListener('click', userSelectRecipe)
+        div.append(liRecipeTitle)
+        searchResultContainer.append(div)
+      }
+    })
 }
 
 function userSelectRecipe(event) {
-    if (!event.target.matches('.titleId')) {
-        return
-    }
-    var receipeEventListener = event.target.getAttribute('data-recipeId')
-    var recipeUrl = 'https://api.spoonacular.com/recipes/' + receipeEventListener + '/information?' + davidsApiKey;
-    fetch(recipeUrl)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            searchResultContainer.classList.add('hidden')
-            recipeContainer.classList.remove('hidden')
-            recipeContainer.innerHTML = '';
-            console.log(data);
-            var title = data.title;
-            getSavedresult(title)
-            var recipeImage = data.image;
-            var summary = data.summary;
-            var recipeSteps = data.instructions;
-            var ingredientsArray = data.extendedIngredients;
-            var p = document.createElement('p');
-            var p2 = document.createElement('p');
-            var h2 = document.createElement('h2');
-            var image = document.createElement('img');
-            var liRecipeInformation = document.createElement('p');
-            liRecipeInformation.innerHTML = recipeSteps
-            titleForRecipe = title
-            image.setAttribute('src', recipeImage);
-            h2.append(titleForRecipe)
-            p.innerHTML = summary
-            let btn = document.createElement("button");
-            btn.innerHTML = "Click Me";
-            recipeContainer.append(h2, image, p, liRecipeInformation)
-            document.appendChild(btn);
+  if (!event.target.matches('.titleId')) {
+    return
+  }
+  var receipeEventListener = event.target.getAttribute('data-recipeId')
+  var recipeUrl = 'https://api.spoonacular.com/recipes/' + receipeEventListener + '/information?' + davidsApiKey;
+  fetch(recipeUrl)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      searchResultContainer.classList.add('hidden')
+      recipeContainer.classList.remove('hidden')
+      recipeContainer.innerHTML = '';
+      console.log(data);
+      var title = data.title;
+      getSavedresult(title)
+      var recipeImage = data.image;
+      var summary = data.summary;
+      var recipeSteps = data.instructions;
+      var ingredientsArray = data.extendedIngredients;
+      var p = document.createElement('p');
+      var p2 = document.createElement('p');
+      var h2 = document.createElement('h2');
+      var image = document.createElement('img');
+      var liRecipeInformation = document.createElement('p');
+      liRecipeInformation.innerHTML = recipeSteps
+      titleForRecipe = title
+      image.setAttribute('src', recipeImage);
+      h2.append(titleForRecipe)
+      p.innerHTML = summary
+      recipeContainer.append(h2, image, p, liRecipeInformation)
 
-            for (let index = 0; index < ingredientsArray.length; index++) {
-                const ingredients = ingredientsArray[index].original;
-                var liIngredients = document.createElement('li');
-                liIngredients.append(ingredients);
-                recipeContainer.append(liIngredients)
-            }
-        })
+      for (let index = 0; index < ingredientsArray.length; index++) {
+        const ingredients = ingredientsArray[index].original;
+        var liIngredients = document.createElement('li');
+        liIngredients.append(ingredients);
+        recipeContainer.append(liIngredients)
+        console.log(ingredients);
+      }
+    })
 }
 
 function getSavedresult(recipeName) {
-    console.log(recipeName);
-    //as the user clicks on the button, push the title into the empty array for local storage
-    recipeHistory.push(recipeName)
-    //will also need to create a button
-    localStorage.setItem('savedRecipe', JSON.stringify(recipeHistory))
+  console.log(recipeName);
+  //as the user clicks on the button, push the title into the empty array for local storage
+  recipeHistory.push(recipeName)
+  //will also need to create a button
+  localStorage.setItem('savedRecipe', JSON.stringify(recipeHistory))
 }
 searchForm.addEventListener('submit', handleUserInput);
 
